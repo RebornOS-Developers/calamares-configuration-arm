b8d21a3e7a1d2da16b01ca89af7bd7f6a8adeec5
Rippanda12 <minecraftbgame@abv.bg>
2022-10-23 08:47:22 +0000

    fix netinst

--- post_install/pre_post_install.sh
+++ post_install/pre_post_install.sh
@@ -13,6 +13,15 @@ if [ -f /tmp/lxqt-user ]; then
     echo "LXQt detected..."
 else
     echo "LXQt not detected..."
+    rm /etc/systemd/system/display-manager.service || true
+    #try to enable common display managers but dont crash if they are not installed
+    systemctl enable lightdm.service || true
+    systemctl enable gdm.service || true
+    systemctl enable lxdm.service || true
+    systemctl enable slim.service || true
+    systemctl enable mdm.service || true
+    systemctl enable sddm.service || true
     echo "Removinging LXQt..."
     pacman -Rncsu --noconfirm rebornos-cosmic-lxqt
+
 fi 
